82f44f6b1c1389bd80a2da7c80b91ec3068b1b1d
Greg Trasuk <trasukg@trasuk.com>
2014-01-20 02:15:32 -0500

    Client-profile command line admin app now exists. Files in product/root/bin now have correct file modes on Unix. 'run.sh' is renamed to 'run'. river-container-core 'core-config.xml' is updated to include the river-hsm jar in runtime classpath.

---
+++ admin-app/src/main/java/org/apache/river/container/admin/app/CommandLineAdmin.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.apache.river.container.admin.app;
+
+
+
+/**
+ *
+ * @author trasukg
+ */
+public class CommandLineAdmin {
+    public static void main(String[] args) {
+        System.out.println("Command line admin invoked.");
+        System.exit(0);
+    }
+}
